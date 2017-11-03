8e986dfc13c382ae5638df967922930ba4622b58
Tom Schutte <tjschutte@wisc.edu>
2017-11-03 16:02:00 -0500

    Server notification services working.
    Signed-off-by: Tom Schutte <tjschutte@wisc.edu>

---
+++ Server/src/utilities/NotificationHandler.java
@@ -0,0 +1,60 @@
+package utilities;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class NotificationHandler {
+	
+	private final String API_KEY = "AAAAL2PRmqE:APA91bEQ0DxLElgVjsYzuRqi1gyN1r5ly7Ri8ijoqJVDuyvS67ChgpRqCEYTl4uT0kyGpLDc0_p_4VSXLMKDFxdHtPzABt3NJ6NERkLsmM7J-U4Gfc0M0jVCsKGpvgodVcAAmYmfUsLE ";
+	private final String API_URL_FCM = "https://fcm.googleapis.com/fcm/send";
+
+	public void sendPushNotification(String deviceToken, String notificationTitle, String notificationBody) 
+			throws IOException, JSONException {
+        URL url = new URL(API_URL_FCM);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+ 
+        conn.setUseCaches(false);
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+ 
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Authorization", "key=" + API_KEY);
+        conn.setRequestProperty("Content-Type", "application/json");
+ 
+        JSONObject json = new JSONObject();
+ 
+        json.put("to", deviceToken.trim());
+        JSONObject info = new JSONObject();
+        info.put("title", notificationTitle);
+        info.put("body", notificationBody);
+        json.put("notification", info);
+        try {
+            OutputStreamWriter wr = new OutputStreamWriter(
+                    conn.getOutputStream());
+            wr.write(json.toString());
+            wr.flush();
+ 
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+ 
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        System.out.println("GCM Notification is sent successfully");
+ 
+	}
+
+}

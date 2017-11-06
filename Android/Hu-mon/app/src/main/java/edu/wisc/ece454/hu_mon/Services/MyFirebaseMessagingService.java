1f8d5c76eff1f87d4911e8a26a08d36f9d4eee0f
Tom Schutte <tjschutte@wisc.edu>
2017-11-06 12:30:13 -0600

    Notifications now have dummy accept / decline buttons when the message has a data payload.
    This way a user can accept / decline a friend / battle request from the notification
    dropdown.
    
    Signed-off-by: Tom Schutte <tjschutte@wisc.edu>

--- Android/Hu-mon/app/src/main/java/edu/wisc/ece454/hu_mon/Services/MyFirebaseMessagingService.java
+++ Android/Hu-mon/app/src/main/java/edu/wisc/ece454/hu_mon/Services/MyFirebaseMessagingService.java
@@ -11,6 +11,8 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
+
 import edu.wisc.ece454.hu_mon.Activities.MenuActivity;
 import edu.wisc.ece454.hu_mon.R;
 
@@ -19,14 +21,13 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "MyFirebaseMsgService";
 
     /**
-     * Called when message is received.
+     * Called when message is received. Notification messages are only received here in onMessageReceived when the app
+     * is in the foreground
      *
      * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
      */
-    // [START receive_message]
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // [START_EXCLUDE]
         // There are two types of messages data messages and notification messages. Data messages are handled
         // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
         // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
@@ -34,23 +35,38 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // When the user taps on the notification they are returned to the app. Messages containing both notification
         // and data payloads are treated as notification messages. The Firebase console always sends notification
         // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-        // [END_EXCLUDE]
 
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
-
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
 
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                //scheduleJob();
-            } else {
-                // Handle message within 10 seconds
-                handleNow();
+            Map<String, String> data;
+            data = remoteMessage.getData();
+
+            if (data.containsKey("FRIEND-REQUEST")) {
+                //TODO: Something with pending intents to accept / decline
+            } else if (data.containsKey("BATTLE-REQUEST")) {
+                //TODO: Something with pending intents to accept / decline
             }
+
+            // If there was a data payload it was a request (friend / battle)
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            Intent notificationIntent = new Intent(this, MenuActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+            Notification notification = new Notification.Builder(this)
+                    .setContentTitle(remoteMessage.getNotification().getTitle())
+                    .setContentText(remoteMessage.getNotification().getBody())
+                    .setContentIntent(pendingIntent) // This is the activity they are brought to if they click the notification
+                                                     // Not the activity they are brought to if they click a button.
+                    .setSmallIcon(R.drawable.common_google_signin_btn_icon_light_normal_background)
+                    .addAction(R.drawable.common_google_signin_btn_icon_light, "Accept", null) // Accept the request
+                    .addAction(R.drawable.common_google_signin_btn_icon_light, "Decline", null) // decline the request
+                    .build();
+            mNotificationManager.notify(69, notification);
 
         }
 
@@ -61,23 +77,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
-    }
-    // [END receive_message]
-
-
-    /**
-     * Handle time allotted to BroadcastReceivers.
-     */
-    private void handleNow() {
-        Log.d(TAG, "Short lived task is done.");
+        // TODO: Show foreground notification that doesnt have data payload
     }
 
     /**
-     * Create and show a simple notification containing the received FCM message.
-     *
+     * Create and show a simple notification containing the received FCM message
      * @param messageBody FCM message body received.
      */
     private void sendNotification(String messageBody) {
+        // TODO: Find if / where this is used
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 

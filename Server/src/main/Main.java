7f12cc0bed6e27a587362a551b376425cc4fd4f6
Tom Schutte <tjschutte@wisc.edu>
2017-11-28 22:03:33 -0600

    Config server before run.
    More changes because bordem.
    
    Signed-off-by: Tom Schutte <tjschutte@wisc.edu>

--- Server/src/main/Main.java
+++ Server/src/main/Main.java
@@ -1,18 +1,17 @@
 package main;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.sql.SQLException;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 
 import http.HttpConnectionListener;
 import server.ServerConnectionListener;
@@ -20,51 +19,51 @@ import utilities.JTextAreaOutputStream;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, SQLException {
-		Options options = new Options();
-
-		options.addOption("drop", false, "Drop any preexisting tables in the databases.");
-		options.addOption("test", false, "Attempt to load test data into the databases.");
-		options.addOption("cmd", false, "Run in command line instead of openning a GUI.");
+	public static void main(String[] args) throws IOException, SQLException  {
 		
-		// create the parser
-	    CommandLineParser parser = new DefaultParser();
-	    try {
-	        // parse the command line arguments
-	        CommandLine line = parser.parse( options, args );
-	        
-	        boolean dropTables;
-	        boolean testData;
-	        
-	        // If they ask for gui, give it to them.
-	        if (!line.hasOption("cmd")) {
-	        	JFrame frame = new JFrame("Humon Server");
-				JTextArea messageArea = new JTextArea(20, 60);
-				// Layout GUI
-				messageArea.setEditable(false);
-				frame.getContentPane().add(new JScrollPane(messageArea), "Center");
-				
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.pack();
-				frame.setVisible(true);
-				
-				JTextAreaOutputStream out = new JTextAreaOutputStream (messageArea);
-		        System.setOut (new PrintStream (out));
-	        }
-	        
-	        dropTables = line.hasOption("drop");
-	        testData = line.hasOption("test");
-	        
+
+		JFrame frame = new JFrame("Humon Server");
+		JTextArea messageArea = new JTextArea(20, 60);
+		// Layout GUI
+		messageArea.setEditable(false);
+		frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+		
+		JTextAreaOutputStream out = new JTextAreaOutputStream(messageArea);
+		System.setOut(new PrintStream(out));
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		
+		JPanel config = new JPanel();
+
+		config.setSize(40, 80);
+		
+		final JCheckBox dropTablesCheckBox = new JCheckBox("Drop existing database tables?");
+		final JCheckBox testDataCheckBox = new JCheckBox("Insert test data into database tables?");
+		final JButton start = new JButton("Start");
+		config.add(dropTablesCheckBox);
+		config.add(testDataCheckBox);
+		config.add(start);
+		
+		frame.getContentPane().add(config, "South");
+		frame.setVisible(true);
+		
+		start.addActionListener(new ActionListener() {
 			
-			new ServerConnectionListener(dropTables, testData).start();
-			new HttpConnectionListener().start();
-	        
-	        
-	    }
-	    catch( ParseException exp ) {
-	        // oops, something went wrong
-	        System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
-	    }
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				boolean dropTables;
+				boolean testData;
+				
+				dropTables = dropTablesCheckBox.isSelected();
+				testData = testDataCheckBox.isSelected();
+				start.setEnabled(false);
+
+				new ServerConnectionListener(dropTables, testData).start();
+				new HttpConnectionListener().start();
+				
+			}
+		});
 	}
 }
 
